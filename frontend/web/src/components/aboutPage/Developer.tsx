b0f36483ac17f91b7b80b790005f4dbcb9448f63
catadragon <steinitzchess2311web@users.noreply.github.com>
2026-02-05 23:18:51 -0500

    Add developer images

--- frontend/web/src/components/aboutPage/Developer.tsx
+++ frontend/web/src/components/aboutPage/Developer.tsx
@@ -4,14 +4,33 @@ const Developer = () => {
   return (
     <div
       style={{
-        height: "200px",
         width: "100%",
         maxWidth: "100%",
         border: "2px solid #000",
         marginBottom: "20px",
+        padding: "16px",
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        alignItems: "center",
+        gap: "16px",
         boxSizing: "border-box",
       }}
-    />
+    >
+      <div style={{ display: "flex", justifyContent: "flex-start" }}>
+        <img
+          src="/assets/developers/CataDragon.png"
+          alt="CataDragon"
+          style={{ maxWidth: "100%", height: "auto", display: "block" }}
+        />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img
+          src="/assets/developers/ChessNut.jpg"
+          alt="ChessNut"
+          style={{ maxWidth: "100%", height: "auto", display: "block" }}
+        />
+      </div>
+    </div>
   );
 };
 
