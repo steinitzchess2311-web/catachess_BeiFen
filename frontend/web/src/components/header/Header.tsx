2608453b69880637bd2176c1ca82f7e97b7568bf
catadragon <steinitzchess2311web@users.noreply.github.com>
2026-02-06 23:26:19 -0500

    Update header nav size and logo

--- frontend/web/src/components/header/Header.tsx
+++ frontend/web/src/components/header/Header.tsx
@@ -14,18 +14,13 @@ const Header: React.FC<HeaderProps> = ({ username, isAuthed }) => {
     <header className="app-header">
       <div className="header-left">
         <Link to="/" className="logo">
-          <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 2L2 7V17L12 22L22 17V7L12 2Z" stroke="black" strokeWidth="2" strokeLinejoin="round"/>
-            <path d="M2 7L12 12L22 7" stroke="black" strokeWidth="2" strokeLinejoin="round"/>
-            <path d="M12 22V12" stroke="black" strokeWidth="2" strokeLinejoin="round"/>
-          </svg>
-          <span className="app-name">ChessorTag</span>
+          <img src="/assets/logo.jpg" alt="ChessorTag" className="logo-image" />
         </Link>
       </div>
       <nav className="header-center" />
       <div className="header-right">
         <Link to="/players" className="nav-link">Players</Link>
-        <Link to="/about" className="nav-link">About Us</Link>
+        <Link to="/about" className="nav-link">About</Link>
         {isAuthed ? (
           <Link to="/account" className="username">{displayName}</Link>
         ) : (
